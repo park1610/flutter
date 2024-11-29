a03ed890e7251a3dd4d9800e5234564444058c0c
park1610 <pargavan1623@gmail.com>
2024-11-29 17:17:34 +0530

    new screen view

--- lib/home.dart
+++ lib/home.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'cart.dart';
-import 'description.dart';
 import 'details.dart';
 import 'order.dart';
-import 'category.dart'; // Ensure this import points to your Category widget.
+import 'category.dart';
 
 class barathi extends StatefulWidget {
   const barathi({super.key});
 
   @override
-  State<barathi> createState() => _BarathiState();
+  State<barathi> createState() => _barathiState();
 }
 
-class _BarathiState extends State<barathi> {
+class _barathiState extends State<barathi> {
   final List<Map<String, String>> _products = [
     {'image': 'assets/images/Ballchair.jpg', 'name': 'Ballchair'},
     {'image': 'assets/images/Massagechair.jpg', 'name': 'Massagechair'},
@@ -30,9 +29,9 @@ class _BarathiState extends State<barathi> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[500],
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.white70,
+        backgroundColor: Colors.grey[300],
         leading: Builder(
           builder: (context) {
             return IconButton(
@@ -40,7 +39,6 @@ class _BarathiState extends State<barathi> {
               onPressed: () {
                 Scaffold.of(context).openDrawer();
               },
-              
             );
           },
         ),
@@ -73,151 +71,158 @@ class _BarathiState extends State<barathi> {
             ),
           ],
         ),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(9.0),
-          child: Column(
-            children: <Widget>[
-              const SizedBox(height: 20),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  hintText: "Search what you're looking for",
-                  prefixIcon: const Icon(Icons.search),
-                ),
-              ),
-              const SizedBox(height: 10),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: 250,
-                margin: const EdgeInsets.symmetric(horizontal: 7.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  image: const DecorationImage(
-                    image: AssetImage('assets/images/Ballchair.jpg'),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    gradient: LinearGradient(
-                      begin: Alignment.bottomRight,
-                      end: Alignment.topLeft,
-                      colors: [
-                        Colors.black.withOpacity(.4),
-                        Colors.black.withOpacity(.2),
-                      ],
+      ),    
+      body: CustomScrollView(  
+        slivers: [
+          SliverPadding(
+            padding: const EdgeInsets.all(9.0),
+            sliver: SliverToBoxAdapter(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment. start,                
+                children: <Widget> [                  
+                  Text(
+                    'What Would ',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),                    
+                    ),
+                    SizedBox(height: 5,),  
+                    Text(
+                      'You like',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ) ,              
+                    
+                    
+                  const SizedBox(height: 10),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(40),
+                      ),
+                      hintText: "Search you're Looking For",
+                      prefixIcon: const Icon(Icons.search),
                     ),
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      const Text(
-                        'Lifestyle Sale',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 35,
-                          fontWeight: FontWeight.bold,
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    height: 200,
+                    child: Container(                      
+                      padding: EdgeInsets.all(10),                                          
+                      color: Colors.grey[400],
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        physics: const BouncingScrollPhysics(),
+                        itemCount: _products.length,
+                        itemBuilder: (context, index) {
+                          final product = _products[index];
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => Details(
+                                    image: product['image']!,
+                                    imageName: product['name'] ?? 'Unknown Product',
+                                    description:
+                                        'No description available for this product',
+                                    imageprice: imageprice[index],
+                                  ),
+                                ),
+                              );
+                            },
+                            child: Container(
+                              width: MediaQuery.of(context).size.width * 0.5,
+                              margin: const EdgeInsets.symmetric(horizontal: 6.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(40),
+                                image: DecorationImage(
+                                  image: AssetImage(product['image']!),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.all(10.0),
+            sliver: SliverGrid(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                childAspectRatio: 0.75,
+              ),
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  final product = _products[index];
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Details(
+                            image: product['image']!,
+                            imageName: product['name'] ?? 'Unknown Product',
+                            description:
+                                'No description available for this product',
+                            imageprice: imageprice[index],
+                          ),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                          image: AssetImage(product['image']!),
+                          fit: BoxFit.cover,
                         ),
                       ),
-                      const SizedBox(height: 30),
-                      Container(
-                        height: 50,
-                        margin: const EdgeInsets.symmetric(horizontal: 90),
+                      child: Container(
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(20),
-                          color: Colors.white,
+                          color: Colors.black.withOpacity(0.3),
                         ),
-                        child: Center(
-                          child: MaterialButton(
-                            onPressed: () {
-                              Get.to(() => const Category(category: 'All Category'));
-                            },
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: const Text(
-                              "Shop Now",
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontWeight: FontWeight.bold,
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Text(
+                              product['name']!,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,                                
                               ),
                             ),
                           ),
                         ),
                       ),
-                      const SizedBox(height: 20),
-                    ],
-                  ),
-                ),
+                    ),
+                  );
+                },
+                childCount: _products.length,
               ),
-              const SizedBox(height: 20),
-              Expanded(
-                child: ListView.builder(
-                  padding: EdgeInsets.symmetric(horizontal: 5,vertical: 30),                  
-                  scrollDirection: Axis.horizontal,
-                  physics: const BouncingScrollPhysics(),
-                  itemCount: _products.length,
-                  itemBuilder: (context, index) {
-                    final product = _products[index];
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => Details(
-                              image: product['image']!,
-                              imageName: product['name'] ?? 'Unknown Product',
-                              description: 'No description available for this product',
-                              imageprice: imageprice[index],
-                            ),
-                          ),
-                        );
-                      },
-                      child: Container(
-                        width: 200,
-                        margin: const EdgeInsets.symmetric(horizontal: 5),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          image: DecorationImage(
-                            image: AssetImage(product['image']!),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                         child: Transform.translate(
-                                offset: const Offset(70, -120),
-                                child: Container(
-                                  margin: const EdgeInsets.symmetric(
-                                      horizontal: 75, vertical: 125),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(20),
-                                    color: Colors.transparent,
-                                  ),
-                                  child: const Icon(
-                                    Icons.shopping_bag_outlined,
-                                    size: 25,
-                      ),
-                                )
-                         )
-                      )
-                    );
-                  },
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
       bottomNavigationBar: Padding(
         padding: EdgeInsets.zero,
         child: NavigationBar(
-          indicatorColor: Colors.grey,
-          backgroundColor: Colors.white70,
+          backgroundColor: Colors.grey[300],
           onDestinationSelected: (index) {
             if (index == 1) {
               Get.to(() => const Category(category: 'All Category'));
